2e1a127ba4c27ab20221fefb27f99bd55dc9c9d7
kowsika <kowsikaneelakandan@gmail.com>
2025-01-19 22:11:12 +0530

    url change

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
-        secure: false,
+        target: "https://realestate-26kt.onrender.com",
+        changeOrigin: true,
+        secure: false
       },
     },
   },
