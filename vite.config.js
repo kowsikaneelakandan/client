b04df952f503c799fdf4ba6ef2e2efdef94e0349
kowsikaneelakandan <kowsikaneelakandan@gmail.com>
2025-01-27 14:52:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://realestate-26kt.onrender.com",
-        changeOrigin: true,
-        secure: false
+${req.url}`);
+        });
       },
     },
   },
+},
 
   plugins: [react()],
-});
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "https://realestate-26kt.onrender.com",
+//         changeOrigin: true,
+//         secure: false
+//       },
+//     },
+//   },
+
+//   plugins: [react()],
+// });
