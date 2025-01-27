2035ec82d6172b5acdc8a6f781669bea9cf76ecd
kowsikaneelakandan <kowsikaneelakandan@gmail.com>
2025-01-27 15:14:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,23 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
- server: {
-  proxy: {
-    "/api": {
-      target: "https://realestate-26kt.onrender.com",
-      changeOrigin: true,
-      secure: false,
-      configure: (proxy, options) => {
-        proxy.on("proxyReq", (proxyReq, req, res) => {
-          console.log(`[Proxy Request] ${req.method} ${req.url}`);
-        });
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://realestate-26kt.onrender.com",
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, options) => {
+          proxy.on("proxyReq", (proxyReq, req, res) => {
+            console.log(`[Proxy Request] ${req.method} ${req.url}`);
+          });
+        },
       },
     },
   },
-},
-
   plugins: [react()],
-)}
+});
+
 
 // export default defineConfig({
 //   server: {
